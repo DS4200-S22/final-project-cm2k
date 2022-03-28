93b1ef91e4c2272a7d057ce94f80cfff230e999c
katerina <thano.k@northeastern.edu>
2022-03-28 12:08:44 -0400

    map?

--- js/visualization.js
+++ js/visualization.js
@@ -4,3 +4,47 @@ d3.select("#vis-container")
   .attr("x", 20)
   .attr("y", 20)
   .text("Hello World!");
+
+
+  d3.csv('data/us-states-covid-data.csv', function(err, rows){
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+    }
+
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'USA-states',
+        locations: unpack(rows, 'fips'),
+        z: unpack(rows, 'cases'),
+        text: unpack(rows, 'state'),
+        zmin: 0,
+        zmax: 17000,
+        colorscale: [
+            [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
+            [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
+            [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+        ],
+        colorbar: {
+            title: 'Cases',
+            thickness: 0.2
+        },
+        marker: {
+            line:{
+                color: 'rgb(255,255,255)',
+                width: 2
+            }
+        }
+    }];
+
+
+    var layout = {
+        title: 'COVID-19 Cases',
+        geo:{
+            scope: 'usa',
+            showlakes: true,
+            lakecolor: 'rgb(255,255,255)'
+        }
+    };
+
+    Plotly.newPlot("myDiv", data, layout, {showLink: false});
+});
